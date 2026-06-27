0051a9a33c1c8b3fdaf5d857a84e7f872ec21820
sieunssam <sieunssam@users.noreply.github.com>
2026-06-27 20:02:06 +0900

    Bust config cache for Apps Script endpoint

---
+++ landing.config.v2.js
@@ -0,0 +1,5 @@
+window.LANDING_CONFIG = {
+  appScriptUrl: "https://script.google.com/macros/s/AKfycbyjyKcOPAku7Eg4X1L7BESvYNOTBwGAEo4ge3pM_EqkNYl6B1ENs2m1B3ZNBdNuYQ2UaA/exec",
+  phoneNumber: "010-6689-2348",
+  developer: "박시은",
+};
