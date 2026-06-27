22bd6ea9a9408e6e492659d2887c072b1ed6cadf
sieunssam <sieunssam@users.noreply.github.com>
2026-06-27 19:58:45 +0900

    Update Apps Script deployment URL

--- landing.config.js
+++ landing.config.js
@@ -1,5 +1,5 @@
 window.LANDING_CONFIG = {
-  appScriptUrl: "https://script.google.com/macros/s/AKfycbx3Dc9Uychf-j6MvCqAlOYpQe-NLbmDmSlI05RZowoAMITMcvVZ9Vw6AoSegyis_2KnSg/exec",
+  appScriptUrl: "https://script.google.com/macros/s/AKfycbyjyKcOPAku7Eg4X1L7BESvYNOTBwGAEo4ge3pM_EqkNYl6B1ENs2m1B3ZNBdNuYQ2UaA/exec",
   phoneNumber: "010-6689-2348",
   developer: "박시은",
 };
